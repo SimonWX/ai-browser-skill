8e1ecc0b5e3e246b047e9d1398d374b1c09a52f5
xuhao <burncal@163.com>
2026-03-10 11:40:08 +0800

    Second：实现自动打开浏览器和网页，输入框自动输入，终端检测并打印

--- src/actions/typeText.ts
+++ src/actions/typeText.ts
@@ -1,1 +1,4 @@
-
+export async function typeText(page: any, selector: string, text: string) {
+  await page.fill(selector, text);
+  console.log("输入文本", text);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { createBrowser } from "./browser.ts";
 import { openPage } from "./actions/openPage.ts";
+import { typeText } from "./actions/typeText.ts";
 
 async function run() {
   const { browser, page } = await createBrowser();
   await openPage(page, "https://github.com");
+  await typeText(page, "input[name='q']", "openlayers");
 }
 
 run();
